7746f42c048caea4959745ca2ddbd9a3fbd6287d
Simón Arboleda Ramirez <simon0820s@gmail.com>
2021-08-26 11:53:19 -0500

    completado, temporalmente la informacíon

---
+++ js/informacion.js
@@ -0,0 +1,13 @@
+
+window.onload = init;
+
+
+function init(){
+   var menu = document.getElementById("btnMenu").addEventListener("click", btnMenu);
+
+}
+function btnMenu(){
+
+    location.href= "index.html";
+
+}
---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.onload = init;
+
+
+function init(){
+   var contactos = document.getElementById("btnMasky").addEventListener("click", btnMasky);
+   var menu = document.getElementById("btnMenu").addEventListener("click", btnMenu)
+
+
+}
+function btnMasky(){
+
+    location.href= "contactos.html";
+
+}
+function btnMenu(){
+    
+    location.href= "index.html";
+}
