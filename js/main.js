647fc962c19fc52a5409daafc1e17979b7dad70b
Simón Arboleda Ramirez <simon0820s@gmail.com>
2021-08-26 13:38:54 -0500

    contactos falta la flecha

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ window.onload = init;
 
 function init(){
    var contactos = document.getElementById("btnMasky").addEventListener("click", btnMasky);
-   var menu = document.getElementById("btnMenu").addEventListener("click", btnMenu)
+   var menu = document.getElementById("btnContactos").addEventListener("click", btnMenu)
 
 
 }
 function btnMasky(){
 
-    location.href= "contactos.html";
+    location.href= "sobreNosotros.html";
 
 }
 function btnMenu(){
     
-    location.href= "index.html";
+    location.href= "contactos.html";
 }
