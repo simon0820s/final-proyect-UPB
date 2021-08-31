84f843bb70d6e5dd1753bbce2e4bf2e78dc3b772
Simón Arboleda Ramirez <simon0820s@gmail.com>
2021-08-30 19:09:26 -0500

    x

--- js/informacion.js
+++ js/informacion.js
@@ -1,13 +1,35 @@
-
-window.onload = init;
-
-
-function init(){
-   var menu = document.getElementById("btnMenu").addEventListener("click", btnMenu);
-
-}
-function btnMenu(){
-
-    location.href= "index.html";
-
-}
+$(document).ready(function() {
+    setHeight();
+    function setHeight() {
+      windowHeight = ($(window).innerHeight())-170;
+      $('.menu-slider').css('height', windowHeight);
+      
+      // Get the full height of the UL
+      var ulheight = $(".menu-slider ul li")[0].scrollHeight;
+  
+      // Position the UL according to it's height and the current menu height
+      var pushdown = ((windowHeight-140)-ulheight)/2;
+      $(".menu-slider ul").css("padding-top", pushdown);
+    };
+    
+    $(window).resize(function() {
+      setHeight();
+    });
+    (function () {
+      $('.burger-bg').on('click', function() {
+        $( this ).toggleClass('bg-animate');
+        $('.bar').toggleClass('bar-animate');
+        $('.menu-slider').toggleClass('menu-open');
+        setHeight();
+      })
+    })();
+    
+    (function () {
+      $('.bar').on('click', function() {
+        $( this ).toggleClass('bar-animate');
+        $('.burger-bg').toggleClass('bg-animate');
+        $('.menu-slider').toggleClass('menu-open');
+        setHeight();
+      })
+    })();
+  });
